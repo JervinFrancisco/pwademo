7ab48fdde51ff6ba9f21a516ba246fa531998a31
JERVINFRANCISCO <FRAN0436@ALGONQUINLIVE.COM>
2021-10-19 16:43:46 -0400

    changes

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 //Service workers get installed once in their life cycle
 self.addEventListener("install", e =>{
-    e.waitUntill(
+    e.waitUntil(
         caches.open("static").then(cache =>{
             return cache.addAll(["./", "./src/master.css", "./images/logo192.png"])
         })
